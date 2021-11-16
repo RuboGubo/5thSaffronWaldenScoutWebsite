7a1acfdc2ca6df33f6037acfdeebe05f7ed7debd
RuboGubo <ruben.john.ward@gmail.com>
2021-11-16 20:07:30 +0000

    work

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ app = Flask(__name__)
 def Home():
     return render_template('Home.html')
 
+@app.route('/Pages/Home/')
+def ContentHome():
+    return render_template('Home.html')
+
+@app.route('/Pages/Explorers/VideosFromCamp')
+def ExplorersVideosFromCamp():
+    return "<style>html{width: 100%; font-family: 'Courier New', Courier, monospace;}</style>" + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'*100
 
 # API Module
 @app.route('/API/<ProgramID>/')
-def background_process_test(ProgramID):
+def API(ProgramID):
     log.debug ("ProgramID=" + ProgramID)
     
     ProgramID = int(ProgramID)
@@ -28,7 +35,7 @@ def background_process_test(ProgramID):
     else:
         log.debug('error')
     
-    return ("Running Program ID:  " + str(ProgramID))
+    return ("<style>html{width: 100%; font-family: 'Courier New', Courier, monospace;}</style>Running Program ID:  " + str(ProgramID))
 
 
 if __name__ == '__main__':
